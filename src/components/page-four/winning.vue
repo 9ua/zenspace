b9826f6b808dbaa3383aed6d8ba6cb0d9c2025b8
BS201803302005Y\Stan <9ua9ua@gmail.com>
2018-04-30 18:51:04 +0800

    add 0430 fin

--- src/components/page-four/winning.vue
+++ src/components/page-four/winning.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="winning">
     <ul v-show="showFlag">
-      <li v-for="(item,index) in haa" :key="index" @click="selectFood(item,$event)">
+      <li v-for="(item,index) in winpool" :key="index" @click="selectFood(item,$event)">
         <div class="winning-left">
           <img src="../../assets/img/home/one/ah11x5.png" />
           <div class="winning-xq">
@@ -11,7 +11,7 @@
             </p>
             <p>
               <span>喜中</span>
-              <span>￥{{index+10000.15 | keepTwoNum}}</span>
+              <span>￥{{item.money | keepTwoNum}}</span>
             </p>
           </div>
         </div>
@@ -25,34 +25,36 @@
 </template>
 <script>
 	import pop from './pop'
+import index from 'vue';
   export default {
     data() {
       return {
+        b:0,
         haa: [
         	{name:'william',money:95.08,lotterylist:'大发快3'},
         	{name:'daniel',money:1238.654,lotterylist:'安徽快3'},
-        	{name:'江若泊',money:853482.66,lotterylist:'大发快3'},
+        	{name:'江若泊',money:853.66,lotterylist:'大发快3'},
         	{name:'james',money:452.442,lotterylist:'北京赛车'},
         	{name:'noah',money:45.244,lotterylist:'北京PK10'},
         	{name:'andrew',money:85.17,lotterylist:'香港六合彩'},
         	{name:'samuel',money:85.17,lotterylist:'上海快3'},
         	{name:'david',money:132.04,lotterylist:'大发快3'},
         	{name:'sophia',money:158.25,lotterylist:'大发快3'},
-          {name:'江若泊',money:12057.11,lotterylist:'大发快3'},
+          {name:'江若泊',money:1205.11,lotterylist:'大发快3'},
           {name:'anthony',money:458.23,lotterylist:'大发快3'},
-        	{name:'elizabeth',money:54999.08,lotterylist:'大发快3'},
+        	{name:'elizabeth',money:5499.08,lotterylist:'大发快3'},
         	{name:'christian',money:77.654,lotterylist:'大发快3'},
         	{name:'brandon',money:856.07,lotterylist:'大发快3'},
-        	{name:'zachary',money:12057.654,lotterylist:'大发快3'},
+        	{name:'zachary',money:1207.654,lotterylist:'大发快3'},
         	{name:'jessica,',money:21.36,lotterylist:'大发快3'},
         	{name:'robert',money:31.55,lotterylist:'大发快3'},
         	{name:'Justin',money:31.55,lotterylist:'大发快3'},
         	{name:'Hunter',money:1587.24,lotterylist:'大发快3'},
           {name:'Victoria',money:13.14,lotterylist:'大发快3'},
-          {name:'若烟',money:45244.28,lotterylist:'大发快3'},
-        	{name:'adfas',money:7591452.00,lotterylist:'大发快3'},
-        	{name:'Jasmine',money:853482.66,lotterylist:'大发快3'},
-        	{name:'霍去病',money:95345.03,lotterylist:'大发快3'},
+          {name:'若烟',money:4524.28,lotterylist:'大发快3'},
+        	{name:'adfas',money:7452.00,lotterylist:'大发快3'},
+        	{name:'Jasmine',money:8532.66,lotterylist:'大发快3'},
+        	{name:'霍去病',money:9535.03,lotterylist:'大发快3'},
         	{name:'fdsawqg',money:156877.10,lotterylist:'大发快3'},
         	{name:'gfukhgj',money:1563.20,lotterylist:'大发快3'},
         	{name:'dfhteuj',money:15687.88,lotterylist:'大发快3'},
@@ -60,7 +62,7 @@
         	{name:'ytmioi',money:89.12,lotterylist:'大发快3'},
           {name:'nkkrtuy',money:7552.12,lotterylist:'大发快3'},
           {name:'hfdw6mm',money:1356.16,lotterylist:'大发快3'},
-        	{name:'kkkkksfgfdsg',money:1555535.00,lotterylist:'大发快3'},
+        	{name:'kkkkksfgfdsg',money:1535.00,lotterylist:'大发快3'},
         	{name:'ewdghtr',money:15687.88,lotterylist:'大发快3'},
         	{name:'rey',money:26.12,lotterylist:'大发快3'},
         	{name:'江若泊',money:45.13,lotterylist:'大发快3'},
@@ -71,8 +73,21 @@
         	{name:'江若泊',money:465.577,lotterylist:'大发快3'},
         ],
         selectedFood: {},
+        winpool:[
+          {name:'william',money:95.08,lotterylist:'大发快3'},
+        	{name:'daniel',money:1238.654,lotterylist:'安徽快3'},
+        	{name:'江若泊',money:853482.66,lotterylist:'大发快3'},
+        	{name:'james',money:452.442,lotterylist:'北京赛车'},
+        	{name:'noah',money:45.244,lotterylist:'北京PK10'},
+        	{name:'andrew',money:85.17,lotterylist:'香港六合彩'},
+        	{name:'samuel',money:85.17,lotterylist:'上海快3'},
+        	{name:'david',money:132.04,lotterylist:'大发快3'},
+        	{name:'sophia',money:158.25,lotterylist:'大发快3'},
+          {name:'江若泊',money:12057.11,lotterylist:'大发快3'},
+          ],
         showFlag: true,
         a:0,
+        x:0,
       }
     },
     //可以倒计时，或做秒钟
@@ -81,11 +96,25 @@
           this.a = 1;
           let max = 798121.51545;
           let min = 100;
+          this.x = Math.random(this.a)*3500;
+          this.b = Math.floor(Math.random(this.a)*10+1);
+          this.winpool.reverse();
+          this.winpool.push(this.haa[Math.floor(Math.random(this.a)*10+1)]);
+          console.log(this.winpool);
+          this.winpool[10].money = this.x;
+          this.winpool.shift();
+          this.winpool.reverse();
+          // this.a = Math.floor(Math.random(this.a)*(max-min+1)+min)
+          // this.winpool.push();
+          // this.winpool.shift();
+     
+          console.log(this.x);
+
         //随机整数数
           this.a = Math.floor(Math.random(this.a)*(max-min+1)+min)
-          setTimeout(countdown, 1000)
+          setTimeout(countdown, 2000)
       }
-      setTimeout(countdown, 1000)
+      setTimeout(countdown, 2000)
     },
     methods: {
       selectFood(item, event) {

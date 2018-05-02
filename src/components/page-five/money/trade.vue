c5f34873ad0e4ced2ede3618a3463bf5552408d3
BS201803302005Y\Stan <9ua9ua@gmail.com>
2018-05-02 19:47:35 +0800

    stan 0502

--- src/components/page-five/money/trade.vue
+++ src/components/page-five/money/trade.vue
@@ -11,8 +11,8 @@
         <van-actionsheet class="mIcode-go" v-model="show" :actions="actions" cancel-text="取消">
         </van-actionsheet>
       </div>
-	  <van-tabs v-model="active">
-  		<van-tab class="typeo" v-for="item in pagelist" :title="item.name">
+	  <van-tabs v-model="accountChangeType" @click="print">
+  		<van-tab class="typeo" v-for="(item,index) in pagelist" :key="index" :title="item.name">
    			 <!-- 内容 {{ item.name }} -->
   		</van-tab>
 	  </van-tabs>
@@ -23,10 +23,11 @@
 export default {
   data(){
     return {
-		active: 2,
+		    active: 1,
         timeline:'今日',
         show:false,
-        show2:false,
+        betweenType:1,
+        accountChangeType:100,
         usertype:2,
         highbet:0,
         rebateratio:0,
@@ -36,29 +37,38 @@ export default {
         invitelist:'',
         selected:[],
 		showFlag: true,
-		pagelist:[
-			{
-				name:'所有类型'
-			},
-			{
-				name:'提现记录'
-			},
-			{
-				name:'充值记录'
-			}
-		],
+          pagelist:[
+          {
+            name:'所有类型',
+            Type:100,
+            callback: this.print,
+          },
+          {
+            name:'提现记录',
+            Type:1,
+            callback: this.print,
+          },
+          {
+            name:'充值记录',
+            Type:2,
+            callback: this.print,
+          }
+        ],
         actions: [
         {
           name: '今天',
+          Type:1,
           callback: this.onClick,
         },
         {
           name: '昨天',
+          Type:2,
           callback: this.onClick,
         //   subname: '描述信息'
         },
         {
           name: '七天',
+          Type:3,
           callback: this.onClick,
           loading: false
         },
@@ -68,9 +78,30 @@ export default {
     }
   },
   methods: {
-    onClick(name){
-      this.timeline = name.name;
+    onClick(item){
+      this.timeline = item.name;
+      this.betweenType = item.Type;
       this.show = ! this.show;
+      console.log(this.betweenType);
+      this.getTradeList();
+    },
+    print(index,title){
+      this.accountChangeType = this.pagelist[index].Type;
+      console.log(this.pagelist[index].Type);
+      this.getTradeList();
+    },
+    getTradeList(){
+        this.$http.get(this.$store.state.url+'api/proxy/getTradeList',{params:{account:this.$store.state.Globalusername,include:3,accountChangeType:this.accountChangeType,betweenType:this.betweenType,}}).then((res) => {
+            console.log(res);
+            // this.highbet = res.data.data.rebateRatio;
+            // for(let i = res.data.data.rebateRatio*10; i >= 0 ; i = i - 1 ){this.betlist.push(i/10)};
+            //     console.log(this.betlist)
+            //     return this.betlist;
+            //         console.log(res.data.data.rebateRatio);
+			}).catch((error) => {
+                console.log(error);
+                    console.log("获取彩種ratio ERROR");
+		});
     },
   },
 };

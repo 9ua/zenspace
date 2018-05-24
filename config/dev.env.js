784de66d93c11512f31207fd1b57f509157f49f6
wtopull <471601494@qq.com>
2018-05-24 11:59:57 +0800

    dsaf

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: 'http://115.144.238.217/'
+  // API_ROOT: '"http://115.144.238.217/"'
 })

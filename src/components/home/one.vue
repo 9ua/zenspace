dadb073a5f31feeb82afa7ac3cdcac30a3224e68
wtopull <471601494@qq.com>
2018-05-26 21:44:43 +0800

    fasfasfasdfsaf nathan

--- src/components/home/one.vue
+++ src/components/home/one.vue
@@ -46,26 +46,27 @@
 		// },
     methods:{
     	getLotterlist(){
-        if(localStorage.getItem('indexInfo') !== null){
-          this.lotteryList = JSON.parse(localStorage.getItem('indexInfo')).hotLotterys;
-          this.bannerList = JSON.parse(localStorage.getItem('indexInfo')).banners;
-          for (let i = 0 ; i < JSON.parse(localStorage.getItem('indexInfo')).noticeList.length; i++) {
-                  this.title = this.title +"   "+ JSON.parse(localStorage.getItem('indexInfo')).noticeList[i].title;
-                }
-        } else {
-          this.$http.get(this.$store.state.url+'api/index/getIndexInfo').then((res) => {
-                this.title = '';
-                localStorage.setItem('indexInfo',JSON.stringify(res.data.data)); 
-                this.lotteryList = res.data.data.hotLotterys;
-                this.bannerList = res.data.data.banners;
-                for (let i = 0 ; i < res.data.data.noticeList.length; i++) {
-                  this.title = this.title +"   "+ res.data.data.noticeList[i].title;
-                }
-          }).catch((error) => {
-            console.log("No")
-        })
-        }
-      }
+				// if(localStorage.getItem('indexInfo') !== null){
+				// 	this.lotteryList = JSON.parse(localStorage.getItem('indexInfo')).hotLotterys;
+				// 	this.bannerList = JSON.parse(localStorage.getItem('indexInfo')).banners;
+				// 	for (let i = 0 ; i < res.data.data.noticeList.length; i++) {
+				// 		this.title = this.title +"   "+ res.data.data.noticeList[i].title;
+				// 	}
+				// } else {
+					this.$http.get(this.$store.state.url+'api/index/getIndexInfo').then((res) => {
+								this.title = '';
+								// localStorage.setItem('indexInfo',JSON.stringify(res.data.data)); 
+								this.lotteryList = res.data.data.hotLotterys;
+								console.log(res.data.data)
+								this.bannerList = res.data.data.banners;
+								for (let i = 0 ; i < res.data.data.noticeList.length; i++) {
+									this.title = this.title +"   "+ res.data.data.noticeList[i].title;
+								}
+	      	}).catch((error) => {
+	      		console.log("No")
+	      })
+				}
+    	// }
     },
     components:{
     	headers

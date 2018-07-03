be6b2e324c7747dfa95239c46fcba76f582866ae
wtopull <471601494@qq.com>
2018-07-03 15:29:08 +0800

    fdsafdsf

--- src/App.vue
+++ src/App.vue
@@ -22,19 +22,4 @@ html,
 p {
   margin: 0;
 }
-.van-popup{
-  position: inherit !important;
-    background-color: rgba(0, 0, 0, 0.1);
-    -webkit-transform: translate3d(0,0,0) !important;
-    -moz-transform: translate3d(0,0,0) !important;
-    -ms-transform: translate3d(0,0,0) !important;
-    -o-transform: translate3d(0,0,0) !important;
-    transform: translate3d(0,0,0) !important;
-    -webkit-transition: .2s ease-out !important;
-    -moz-transition: .2s ease-out !important;
-    -ms-transform: .2s ease-out !important;
-    -o-transition: .2s ease-out !important;
-    transition: .2s ease-out !important;
-    
-}
 </style>

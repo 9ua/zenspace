e4767ec8c23ceb9c8c955d608293f56accacf499
john <wangyigma@gmail.com>
2018-07-30 16:17:32 +0800

    john

--- src/components/public/actionSheet.vue
+++ src/components/public/actionSheet.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class="modal" @touchmove.prevent.stop @click="$emit('hide', $event)" v-show='value'>
-    <transition name="actionsheet-float"  v-show='value'>
-      <div class="actionsheet" :class="{ 'actionsheet_title': title }"  v-show='value'>
-        <div class="actionsheet-header" v-if="title">
-          <div v-text="title" />
-          <i class="iconfont icon-close" @click.stop="$emit('hide', false)" />
-        </div>
-        <ul v-if="!title" class="actionsheet-list">
-
-        </ul>
-        <div v-if="cancelText" v-text="cancelText" class="actionsheet-item actionsheet-cancel" @click.stop="$emit('hide', false)" />
-        <div v-else class="actionsheet-content">
-          <slot />
-        </div>
+  <transition name="actionsheet-float" v-show='value'>
+    <div class="actionsheet" :class="{ 'actionsheet_title': title }" v-show='value'>
+      <div class="actionsheet-header" v-if="title">
+        <div v-text="title" />
+        <i class="iconfont icon-cuo" @click.stop="$emit('hide', false)" />
       </div>
-    </transition>
-  </div>
+      <ul v-if="!title" class="actionsheet-list">
+
+      </ul>
+      <div v-if="cancelText" v-text="cancelText" class="actionsheet-item actionsheet-cancel" @click.stop="$emit('hide', false)" />
+      <div v-else class="actionsheet-content">
+        <slot />
+      </div>
+    </div>
+  </transition>
 </template>
 <script>
+import Modal from "./Modal";
+import Vue from "vue";
 export default {
   name: "actionsheet",
   props: {
@@ -33,11 +33,27 @@ export default {
       default: true
     }
   },
+  watch: {
+    value(val) {
+      this.update();
+    }
+  },
+  beforeDestroy(){
+    document.body.removeChild(document.querySelector(".modal"));
+  },
   methods: {
-    onClickItem(item) {
-      if (typeof item.callback === "function") {
-        item.callback(item);
+    update() {
+      if (!document.querySelector(".modal")) {
+          let modal = new (Vue.extend(Modal))({
+          el: document.createElement("div")
+        });
+        modal.$on("click", this.onClick);
+        document.body.appendChild(modal.$el);
       }
+      document.querySelector(".modal").style.display=this.value ? "block" : "none";
+    },
+    onClick() {
+      this.$emit("hide",false);           
     }
   }
 };
@@ -54,13 +70,14 @@ export default {
   transition: 0.2s ease-out;
   &-float-enter,
   &-float-leave-active {
-    transform: translate3d(0, 100%, 0);
+    transform: translateY(100%);
   }
+
   &-header {
     line-height: 44px;
     text-align: center;
     position: relative;
-    & .icon-close {
+    & .iconfont {
       top: 0;
       right: 0;
       padding: 0 0.4rem;
@@ -70,32 +87,5 @@ export default {
       line-height: inherit;
     }
   }
-}
-@keyframes bottom-enter {
-  from {
-    transform: translate3d(0, 0, 0);
-  }
-  to {
-    transform: translate3d(0, 100%, 0);
-  }
-}
-
-@keyframes bottom-leave {
-  from {
-    transform: translate3d(0, 100%, 0);
-  }
-  to {
-    transform: translate3d(0, 0, 0);
-  }
-}
-.modal {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  transition: opacity 1.2s ease-out;
-  background-color: rgba(0, 0, 0, 0.2);
-  z-index: 2000;
 }
 </style>

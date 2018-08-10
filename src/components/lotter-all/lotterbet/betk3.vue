e38b9f9a1f9c679a908dac7e75486a7fab894543
Stanley <9ua9ua@gmail.com>
2018-08-10 15:08:03 +0800

    123

--- src/components/lotter-all/lotterbet/betk3.vue
+++ src/components/lotter-all/lotterbet/betk3.vue
@@ -510,9 +510,6 @@ export default {
         if(document.hidden === false){
           this.geteServerTime();
         }
-        if(document.hidden === true){
-          this.endCount();
-        }
     },
     //没打接口前
     noGetItem() {
@@ -636,7 +633,7 @@ export default {
             this.setTimeMode();
             this.initSetTimeout();
             this.getPastOp();
-          }
+          } 
         })
         .catch(error => {
           console.log("获取彩種當前獎期時間No");
